ce9c6362b20fa194611e23110ac163d52a3d429d
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-08-25 18:50:29 -0300

    WIP: add new habit using --habit, handle CLI state in proper class

--- cli.py
+++ cli.py
@@ -1,8 +1,96 @@
 import click 
 import os
+import io
 from random import randrange
+from typing import List
 import json
+import errno
 from datetime import datetime
+
+def open_file(path: str, mode: str): 
+    try: 
+        return open(path, mode)
+    except OSError as error:
+        if error.errno == errno.ENOENT:
+            return None
+
+        print(f"Error trying to open {path} file")
+        print("Aborted!")
+        exit(1)
+
+def overwrite_file(data, f: io.TextIOWrapper):
+    f.seek(0)
+    f.write(data)
+    f.truncate()
+
+class Habit: 
+    def __init__(self, name: str, date: str):
+        self.name = name
+        self.date = date
+
+    def json_serialize(self):
+        return {
+            "name": self.name,
+            "date": self.date
+        }
+
+class Quote:
+    def __init__(self, name: str, content: str):
+        self.name = name
+        self.content = content 
+
+class QuotesData:
+    def __init__(self, quotes: List[Quote]):
+        self.quotes = quotes
+        self.last_quote = {
+            "index": 0,
+            "date": datetime.now().date
+        }
+
+class State:
+    # current open file
+    file: io.TextIOWrapper
+
+    habits: List[Habit] = []
+
+    habits_json: any
+
+    quotes: QuotesData
+    quotes_json: any
+
+    def __init__(self, habit):
+        if (habit):
+            self.file = open_file("habits.json", "r+")
+
+            if (self.file == None):
+                self.file = open_file("habits.json", "x")
+                self.file.close()
+                self.file = open_file("habits.json", "r+")
+                self.habits.append(habit)
+            else:
+                self.habits_json = json.loads(self.file)
+                self.habits = self.habits_json["habits"]
+                self.habits.append(habit)
+
+            habits = {
+                "habits": [] 
+            }
+
+            for h in self.habits:
+                serialized = h.json_serialize()
+                habits["habits"].append(serialized)
+
+            overwrite_file(json.dumps(habits), self.file)
+
+            self.file.close()
+        else:
+            self.file = open_file("habits.json", "r")
+            self.habits_json = json.loads(self.file)
+            self.file.close()
+
+        self.file = open_file("quotes.json", "r")
+        self.quotes_json = json.load(self.file)
+        self.file.close()
 
 def highlighted_text(text, color, fg: bool):
     if fg: 
@@ -11,33 +99,16 @@ def highlighted_text(text, color, fg: bool):
 def move_text(text: str, pos: int):
     return text.rjust(pos)
 
-# TODO: mode type should be OpenTextMode
-def open_file(path: str, mode: str, read_content: bool): 
-    try: 
-        f = open(path, mode)
-
-        if (read_content):
-            data = f.read()
-            f.close()
-            return data
-
-        return f
-    except OSError:
-        print(f"Error trying to open {path} file")
-        exit(-1)
-
 def render_greeting():
-    username = highlighted_text(f"Hello {os.getlogin()}", 'magenta', True)
-    greeting_text = move_text(username, 0)
-    ascii_text = open_file("ascii.txt", "r", read_content=True)
+    username = f"Hello {os.getlogin()}"
+    ascii_text = open_file("ascii.txt", "r")
     click.echo(ascii_text)
-    click.echo(greeting_text)
+    click.echo(username)
     click.echo("\n")
 
 def render_quote():
     # Every day we render a different random quote
-
-    f_read = open_file("quotes.json", "r", read_content=False)
+    f_read = open_file("quotes.json", "r")
     # read+write mode don't work with json.load 
     quotes_data = json.load(f_read)
     f_read.close()
@@ -59,7 +130,7 @@ def render_quote():
     while (rand_quote_index == last_quote_index):
         rand_quote_index = randrange(0, len(quotes)) == last_quote_index
 
-    f_write = open_file("quotes.json", "w", read_content=False)
+    f_write = open_file("quotes.json", "w")
 
     quote = quotes[rand_quote_index]
 
@@ -83,31 +154,38 @@ def render_quote():
     f_write.truncate()
     f_write.close()
 
-def cli():
-    habits = [
-        {
-            "name": "smoking",
-            "date": "2024-08-16"
-        },
-        {
-            "name": "drinking alcohol",
-            "date": "2024-08-16"
-        }
-    ]
+@click.command()
+@click.option("--habit", help='The action or name of the habit, like: "drinking alcohol" or "smoking"')
+def cli(habit):
+    """A CLI app to track your bad habits"""
 
-    render_greeting()
+    state = State(Habit(name=habit, date="2024-08-25"))
+
+    # habits = [
+    #     {
+    #         "name": "smoking",
+    #         "date": "2024-08-16"
+    #     },
+    #     {
+    #         "name": "drinking alcohol",
+    #         "date": "2024-08-16"
+    #     }
+    # ]
+
+    # render_greeting()
     
-    click.echo(" You are:")
-    for habit in habits:
-        today = datetime.now()
-        habit_date = datetime.fromisoformat(habit["date"])
-        difference = today - habit_date
-        name = habit["name"]
-        plural = "days"
-        singular = "day"
-        days = highlighted_text(f" {difference.days} {plural if difference.days > 1 else singular}", "green", True)
-        click.echo(move_text(f"{days} without {name}", 0))
+    # click.echo(" You are:")
+    # for habit in habits:
+    #     today = datetime.now()
+    #     habit_date = datetime.fromisoformat(habit["date"])
+    #     difference = today - habit_date
+    #     name = habit["name"]
+    #     plural = "days"
+    #     singular = "day"
+    #     days = highlighted_text(f" {difference.days} {plural if difference.days > 1 else singular}", "green", True)
+    #     click.echo(move_text(f"{days} without {name}", 0))
 
-    render_quote()
-cli()
+    # render_quote()
 
+if __name__ == "__main__":
+    cli()
